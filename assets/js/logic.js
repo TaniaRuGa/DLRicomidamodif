09efe103413c9fa10410d3cd64c54278bb40be49
TaniaRuGa <158788230+TaniaRuGa@users.noreply.github.com>
2024-08-01 22:43:18 -0400

    Update logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,22 +1,32 @@
+const carousel = document.querySelector('#carouselExample'); // Selecciona el elemento del carrusel con la clase "#carouselExample"//
+carousel.options = 1500; // Ajusta la velocidad del carrusel en 1.5 segundos //
+
 $(document).ready(function() {
-    $('#carouselExample').carousel({
-        interval: 3000 // Cambia el valor para ajustar la velocidad del carrusel
-    });
 
-    $('#enviarCorreo').click(function() {
-        alert('El correo fue enviado correctamente...');
-    });
+  $('#enviarCorreo').click(function() {
+      alert('El correo fue enviado correctamente');
+  });
 
-    $("h4:contains('INGREDIENTES')").on('dblclick', function() {
-        $(this).toggleClass('red-text');
-    });
-       
-    $("h4:contains('PREPARACIÓN')").on('dblclick', function() {
-        $(this).toggleClass('red-text');
-    });
+  $('#agregarFav').click(function() {
+    alert('Agrega esta receta a tus favoritos');
+  });
 
-    $(".card-title").click(function() {
-        $(this).next(".card-text").toggle();
+  $(".txtIngred").dblclick(function() {
+    $(this).css({
+    "color": "red",
     });
+  });
+
+  $(".txtPrep").dblclick(function() {
+    $(this).css({
+    "color": "red",
+    });
+  });
+  
+  $(document).ready(function() {
+    $('.card-title').on('click', function() {
+        $(this).next('.card-text').toggle();
+    });
+  });
 
 });
